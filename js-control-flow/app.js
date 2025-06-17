a43853b045b99e96821d3ac9d66d35dc625bf3ef
mohd <generalassemblyse@gmail.com>
2025-06-17 15:35:15 +0300

    Lesson

--- js-control-flow/app.js
+++ js-control-flow/app.js
@@ -3,3 +3,28 @@ const TestName = "12";
 
 console.log(Name === TestName);
 const test = "this is a test for push";
+
+if(Name === TestName && 12 >= 12 || Name == TestName){
+  console.log("right")
+}
+
+//console.log(12 == Number)
+
+let Testcolor = "Yellow";
+let color = Testcolor.toLowerCase();
+
+if(color === "green"){
+  console.log("Go");
+}else if(color === "yellow"){
+  console.log("Slow");
+}else if(color === "Red"){
+  console.log("Stop");
+}else{
+  console.log("whatever");
+}
+
+for (let number = 1; number <=20; number++) {
+  let squared = number;
+  squared = number * number;
+  console.log(`${number} squared is ${squared}`)
+}
