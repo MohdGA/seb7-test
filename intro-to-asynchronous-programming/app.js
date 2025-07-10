42566b921e811ef6e11c12bb628f4b8e59444623
mohd <generalassemblyse@gmail.com>
2025-07-10 10:26:56 +0300

    reach async-await lesson

---
+++ intro-to-asynchronous-programming/app.js
@@ -0,0 +1,14 @@
+const fs = require('node:fs/promises');
+
+const readDataFiles = async () => {
+  const userData = await fs.readFile('test.txt', 'utf8');
+  console.log(userData);
+  const profileData = await fs.readFile('test2.txt', 'utf8');
+  console.log(profileData);
+  const userHistory = await fs.readFile('test3.txt', 'utf8');
+  console.log(userHistory);
+}
+
+readDataFiles();
+
+console.log('run this as soon as possible');
